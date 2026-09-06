e84a3381bafafa5b2aca7df18c450e34b11cdc70
Pablo Sabater <pabloosabaterr@gmail.com>
2026-09-06 13:44:59 +0100

    main: add --read
    Signed-off-by: Pablo Sabater <pabloosabaterr@gmail.com>

--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,36 @@
+#include "capture.h"
 #include "parse-options.h"
 
 int main(int argc, char **argv)
 {
     bool gen = false;
+    bool read = false;
 
     const option options[] = {
-        OPT_BOOL('g', "gen", &gen),
+        OPT_BOOL('g', "gen",  &gen),
+        OPT_BOOL('r', "read", &read),
     };
 
-    parse_options(argc, argv, options);
+    int i = parse_options(argc, argv, options);
 
-    printf("test = %d\n", gen);
-    return 0;
+    if (gen) {
+        if (argc - i != 2) {
+            fprintf(stderr, "usage: %s --gen <in.jsonl> <out.bin>\n", argv[0]);
+            return 1;
+        }
+        generate_capture(argv[i], argv[i + 1]);
+        return 0;
+    }
+
+    if (read) {
+        if (argc - i != 1) {
+            fprintf(stderr, "usage: %s --read <capture.bin>\n", argv[0]);
+            return 1;
+        }
+        read_capture(argv[i]);
+        return 0;
+    }
+
+    fprintf(stderr, "usage: %s --gen <in.jsonl> <out.bin> | --read <capture.bin>\n", argv[0]);
+    return 1;
 }
