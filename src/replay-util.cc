f23bd2364255fc85ac73839f4de018318b0f4877
Pablo Sabater <pabloosabaterr@gmail.com>
2026-09-06 12:49:21 +0100

    replay-util: write the whole sample
    Write to the given file the data as described in capture.h
    
    Signed-off-by: Pablo Sabater <pabloosabaterr@gmail.com>

--- src/replay-util.cc
+++ src/replay-util.cc
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <cstdio>
+#include <cstring>
 
 namespace {
     constexpr uint8_t BINARY_VERSION = 1;
@@ -10,6 +11,8 @@ namespace {
 void generate_capture(const char* jsonl_path, const char* out_path)
 {
     FILE *file, *to_write;
+    char buffer[1024];
+    uint64_t t = 1000;
 
     file = fopen(jsonl_path, "r");
     if (!file) {
@@ -24,6 +27,19 @@ void generate_capture(const char* jsonl_path, const char* out_path)
     }
 
     fwrite(&BINARY_VERSION, sizeof(BINARY_VERSION), 1, to_write);
+
+    while (fgets(buffer, sizeof(buffer), file)) {
+        uint16_t len = strlen(buffer);
+
+        if (len && buffer[len - 1] == '\n')
+            len--;
+
+        fwrite(&t, sizeof(t), 1, to_write);
+        fwrite(&len, sizeof(len), 1, to_write);
+        fwrite(buffer, 1, len, to_write);
+
+        t += 1000;
+    }
 
     fclose(to_write);
 clean_file:
