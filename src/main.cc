3094305a1ae14a4d0eaa22bd544e83240662cf6b
Pablo Sabater <pabloosabaterr@gmail.com>
2026-09-06 12:01:43 +0100

    replay-util: add generate_capture skeleton
    Open the input JSONL and the output capture file, and write the capture
    header.
    
    Signed-off-by: Pablo Sabater <pabloosabaterr@gmail.com>

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,15 @@
-#include <iostream>
+#include "parse-options.h"
 
-int main() { std::cout << "Hello Adamastor\n"; }
+int main(int argc, char **argv)
+{
+    bool gen = false;
+
+    const option options[] = {
+        OPT_BOOL('g', "gen", &gen),
+    };
+
+    parse_options(argc, argv, options);
+
+    printf("test = %d\n", gen);
+    return 0;
+}
